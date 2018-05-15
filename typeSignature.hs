4f3ac71fa7360d5cd9bbdd69f760fb93bca36bd8
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2018-05-15 15:33:04 +0100

    we can also use guards to specify a series of conditions

--- typeSignature.hs
+++ typeSignature.hs
@@ -12,8 +12,16 @@ increasing :: (Ord a) => [a] -> Bool
 increasing (x:y:ys) = x <= y && increasing(y:ys)
 increasing _ = True
 
+--noVowels :: [Char] -> [Char]
+--noVowels "" = ""
+--noVowels (x:xs) = if x `elem` "aeiouAEIOU"
+--    then noVowels xs
+--    else x : noVowels xs
+--------------------------------------------
+-- we can use guards to specify a series of conditions 
+-- and what we should do in each case
 noVowels :: [Char] -> [Char]
 noVowels "" = ""
-noVowels (x:xs) = if x `elem` "aeiouAEIOU"
-    then noVowels xs
-    else x : noVowels xs
+noVowels (x:xs)
+        | x `elem` "aeiouAEIOU" = noVowels xs
+        | otherwise             = x : noVowels xs
